30c0b882fcec350960d5800943c1cb6a77f92843
Karthicksrini <82822054+Karthicksrini@users.noreply.github.com>
2021-11-14 11:35:36 -0800

    Update brandModule.js

--- Node-inventary/module/brandModule.js
+++ Node-inventary/module/brandModule.js
@@ -1,35 +1,52 @@
-const Brand = require("../models/brand");
 
-exports.postbrand= async(req,res,next)=>{
+const Product = require("../models/Product");
+
+exports.postproduct= async(req,res,next)=>{
     //Object created using model
-    const brand = new Brand ({
+    const product = new Product({
         name:req.body.name,
+        price:req.body.price,
+        quantity:req.body.quantity,
+        description:req.body.description,
+        brands:req.body.brands,
+        colors:req.body.colors,
+        size:req.body.size,
+        category:req.body.category,
+        store:req.body.store,
         status:req.body.status,
     })
     
     //save() function to post data
-    var response = await brand.save();
+    var response = await product.save();
     res.send(response);
 
 }
 
-exports.getbrand=async(req,res,next)=>{
-        var response= await Brand.find();
-        res.send(response);
-    }
+exports.getproduct=async(req,res,next)=>{
+    var response= await Product.find();
+    res.send(response);
+}
 
-exports.updatebrand= async (req,res,next)=>{
+exports.updateproduct= async (req,res,next)=>{
     const id = req.params.id
-    var response = await Brand.findByIdAndUpdate(id,
-        { name:req.body.name,
-          status:req.body.status,
+    var response = await Product.findByIdAndUpdate(id,
+        {   name:req.body.name,
+            price:req.body.price,
+            quantity:req.body.quantity,
+            description:req.body.description,
+            brands:req.body.brands,
+            colors:req.body.colors,
+            size:req.body.size,
+            category:req.body.category,
+            store:req.body.store,
+            status:req.body.status,
 
     })
     res.send(response);
 
 }
 
-exports.deletebrand=async(req,res,next)=>{
-    var response= await Brand.findByIdAndRemove(req.params.id);
+exports.deleteproduct=async(req,res,next)=>{
+    var response= await Product.findByIdAndRemove(req.params.id);
     res.send(response);
 }
